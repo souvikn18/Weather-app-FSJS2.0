ae9f5e304d49f0685c80f4d2590059321d88f278
souvikn18 <souvikn20@gmail.com>
2023-07-29 11:09:01 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("full-location").innerHTML="Kolkata, West Bengal, India";
+
+document.getElementById("main-temp").innerHTML="32";
+document.getElementById("curr-stat").innerHTML="Cloudy";
+
+document.getElementById("curr-loc").innerHTML="Kolkata";
+document.getElementById("max").innerText="34";
+document.getElementById("min").innerHTML="28";
+document.getElementById("date").innerHTML="July 27";
+document.getElementById("day").innerHTML="Thursday";
